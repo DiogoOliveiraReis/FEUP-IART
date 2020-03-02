1917fc6c28ccf219c1f00947e1f4eea81041d342
DiogoOReis <diogo.reis@fe.up.pt>
2020-03-02 18:08:42 +0000

    Initial java file

---
+++ Parquet.java
@@ -0,0 +1,16 @@
+class Parquet {
+    static int[][] board = new int[4][4];
+
+    public static void displayBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        displayBoard(board);
+    }
+}
