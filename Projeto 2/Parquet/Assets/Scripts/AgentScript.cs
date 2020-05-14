d047be2d504560ac1d0417aaeab672abe3d4e64e
Joao Lirio <up201705254@fe.up.pt>
2020-05-14 15:23:05 +0100

    Trying something to train

---
+++ Projeto 2/Parquet/Assets/Scripts/AgentScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class AgentScript : Agent
+{
+    public GameObject board;
+    public GameObject background;
+
+    public Bounds boardBounds;
+
+    ParquetAcademy academy;
+
+    public GameObject goal;
+
+    public GoalDetect goalDetect;
+
+    void Awake()
+    {
+        academy = FindObjectOfType<ParquetAcademy>();
+    }
+
+    public override void InitializeAgent()
+    {
+        base.InitializeAgent();
+        goalDetect = agent.GetComponent<GoalDetect>();
+        goalDetect.agent = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Projeto 2/Parquet/Assets/Scripts/GoalDetect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetect : MonoBehaviour
+{
+    [HideInInspector]
+    public AgentScript agent;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("goal"))
+        {
+            agent.ScoredAGoal();
+        }
+    }
+}
