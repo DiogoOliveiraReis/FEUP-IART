7b997b7e89f55eea1f5fa694bef990454d0024d9
Joao Lirio <up201705254@fe.up.pt>
2020-05-19 18:17:03 +0100

    Two agents now

--- Projeto 2/Parquet/Assets/Scripts/OpponentAgent.cs
+++ Projeto 2/Parquet/Assets/Scripts/OpponentAgent.cs
@@ -7,7 +7,7 @@ using System.Security.AccessControl;
 using System.Security.Cryptography;
 using System.Collections.Specialized;
 
-public class PlayerAgent : Agent
+public class OpponentAgent : Agent
 {
     Rigidbody rBody;
     void Start()
@@ -19,8 +19,8 @@ public class PlayerAgent : Agent
     public Transform VoidPiece1;
     public Transform VoidPiece2;
     public Transform VoidPiece3;
-    private static Vector3 InitialPosition = new Vector3(-250, -250, 0);
-    private static Vector3 InitialTargetPosition = new Vector3(350, 350, 0);
+    private static Vector3 InitialPosition = new Vector3(250, 250, 0);
+    private static Vector3 InitialTargetPosition = new Vector3(-350, -350, 0);
     private static float InitialDistanceToTarget = Vector3.Distance(InitialTargetPosition, InitialPosition);
 
     public override void OnEpisodeBegin()
@@ -31,10 +31,10 @@ public class PlayerAgent : Agent
         this.rBody.angularVelocity = Vector3.zero;
         this.rBody.velocity = Vector3.zero;
         */
-        this.transform.localPosition = new Vector3(-250, -250, 0);
+        this.transform.localPosition = new Vector3(250, 250, 0);
 
         // Move the target to a new spot
-        Target.localPosition = new Vector3(350, 350, 0);
+        Target.localPosition = new Vector3(-350, -350, 0);
         VoidPiece1.localPosition = new Vector3(-50, 50, 0);
         VoidPiece2.localPosition = new Vector3(50, -50, 0);
         VoidPiece3.localPosition = new Vector3(50, 50, 0);
@@ -51,8 +51,8 @@ public class PlayerAgent : Agent
     public override void OnActionReceived(float[] vectorAction)
     {
         Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = (vectorAction[0] * (float)31.80);
-        controlSignal.y = (vectorAction[1] * (float)31.80);
+        controlSignal.x = (vectorAction[0] * (float)-31.80);
+        controlSignal.y = (vectorAction[1] * (float)-31.80);
         rBody.transform.Translate(controlSignal);
 
         // Reached Target
